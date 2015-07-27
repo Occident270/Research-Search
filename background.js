1aafcf8052bfdc037203dfb7e20e9b0b64820ff5
西将誉 <masataka.nishi@mkt-apps.com>
2015-07-27 16:53:35 +0900

    修正

--- background.js
+++ background.js
@@ -6,10 +6,15 @@ chrome.contextMenus.create({
     //選択中の文字列を取得する。
     var research = info.selectionText;
     var url ='https://borders.post-survey.com/admin/research/index.php?research_number='+research;
-    //新しいタブを作成
-    chrome.tabs.create({
-      //Surveyに飛ばす
-      url:url
-    });
+    //リサーチナンバーが選択されているかどうか
+    if(research.match(/^C0[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]$/)){
+      //新しいタブを作成
+      chrome.tabs.create({
+        //Surveyに飛ばす
+        url:url
+      });
+    } else {
+      alert("リサーチナンバー読み取ってーや");
+    }
   }
 });
